9dc0115c51c37eaac373ef16f96bf927c2d54088
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-04-19 02:08:43 +0700

    markup

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDi4TutTz0vVYqkV3NCWqADIOf-JGQ-R00",
+  authDomain: "data-table-fd59e.firebaseapp.com",
+  projectId: "data-table-fd59e",
+  storageBucket: "data-table-fd59e.appspot.com",
+  messagingSenderId: "777360764102",
+  appId: "1:777360764102:web:9f657c7341f5f96bae7bc7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
